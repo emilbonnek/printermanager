9664e6ba8916453ef4b7e11ea19a6bb1a46f170e
Emil Bonne Kristiansen <emilbonnekristiansen@Emils-MacBook-Pro.local>
2015-02-13 21:13:43 +0100

    fixed mistake

--- app/controllers/printers_controller.rb
+++ app/controllers/printers_controller.rb
@@ -5,7 +5,7 @@ class PrintersController < ApplicationController
   # GET /printers.json
   def index
     # @printers = Printer.all.includes(:reservations)
-    @printers = Printer
+    @printers = Printer.all
     @date = params[:date] ||= Date.today
   end
 
